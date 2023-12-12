e8cebaa9fb114da5c1a699a3af29a79a35f47d70
Jeroen Coekaerts <jeroen@dnxnetwork.lu>
2023-12-12 21:37:09 +0100

    alles in class

--- day12b.rb
+++ day12b.rb
@@ -3,9 +3,8 @@
 $debug = false
 
 class Line < Struct.new(:record,:seq)
-    def solve 
-        return solve_line(record,seq)
-    end
+
+    @@cache = {}
 
     def explode
         new_record = []
@@ -19,49 +18,47 @@ class Line < Struct.new(:record,:seq)
         end
         return Line.new(new_record,new_seq)
     end
-end
 
-$cache = {}
-
-
-def cache_solve_line(record,seq)
-    # a||=b means assign b to a only if a is unassigned or evaluates to false
-    return $cache[[record,seq]] ||=  solve_line(record,seq) 
-end
-
-def solve_line(record,seq)
-    
-    if record.empty? then
-        return seq.empty? ? 1 : 0
+    def cache_solve_line()
+        # a||=b means assign b to a only if a is unassigned or evaluates to false
+        return @@cache[[record,seq]] ||=  solve_line() 
     end
 
-    if seq.empty? then
-        return record.include?('#') ? 0 : 1
-    end
+    def solve_line()
+        
+        if record.empty? then
+            return seq.empty? ? 1 : 0
+        end
 
-    if record[0] == '.' then
-        return cache_solve_line(record[1..-1],seq)
-    elsif record[0] == '?' then
-        point_record = (['.'] + record[1..-1])
-        point_solution = solve_line(point_record,seq)
-        hash_record = (['#'] + record[1..-1])
-        hash_solution = solve_line(hash_record,seq)
-        return  point_solution + hash_solution
-    elsif record[0] == '#' then
-        if record.length >= seq[0] and (not record[0..(seq[0]-1)].include?('.')) then
-            if record.length > seq[0]  then
-                if record[seq[0]] == '#' then # +1 -1
-                    return 0
-                else
-                    return cache_solve_line(record[(seq[0]+1)..-1],seq[1..-1]) 
+        if seq.empty? then
+            return record.include?('#') ? 0 : 1
+        end
+
+        if record[0] == '.' then
+            return Line.new(record[1..-1],seq).cache_solve_line()
+        elsif record[0] == '?' then
+            point_record = (['.'] + record[1..-1])
+            point_solution = Line.new(point_record,seq).solve_line()
+            hash_record = (['#'] + record[1..-1])
+            hash_solution = Line.new(hash_record,seq).solve_line()
+            return  point_solution + hash_solution
+        elsif record[0] == '#' then
+            if record.length >= seq[0] and (not record[0..(seq[0]-1)].include?('.')) then
+                if record.length > seq[0]  then
+                    if record[seq[0]] == '#' then # +1 -1
+                        return 0
+                    else
+                        return Line.new(record[(seq[0]+1)..-1],seq[1..-1]).cache_solve_line() 
+                    end
                 end
+                return Line.new(record[seq[0]..-1],seq[1..-1]).cache_solve_line()
+            else
+                return 0
             end
-            return cache_solve_line(record[seq[0]..-1],seq[1..-1])
-        else
-            return 0
         end
     end
 end
+
 
 lines = []
 while line = gets
@@ -69,4 +66,4 @@ while line = gets
     lines.push((Line.new(schema_line.each_char.to_a, broken_sequence.split(',').map { |x| x.to_i })).explode)
 end
 
-puts lines.map { |line| line.solve }.inject(:+)
+puts lines.map { |line| line.solve_line }.inject(:+)
