58ed606f831ee1a2ec241f1cb5d45a4bb1ccf1b2
Jeroen Coekaerts <jeroen@dnxnetwork.lu>
2023-12-12 20:04:19 +0100

    clean up

--- day12b.rb
+++ day12b.rb
@@ -19,15 +19,26 @@ end
 
 def explode_line(line)
     new_record = []
-    for i in 1..5 do
+    5.times do
         new_record = new_record + line.record + ['?']
     end
     new_record.pop
     new_seq = []
-    for i in 1..5 do
+    5.times do
         new_seq = new_seq + line.seq 
     end
     return Line.new(new_record,new_seq)
+end
+
+def cache_solve_line(record,seq)
+    if $cache.has_key?(record) then
+        if $cache[record].has_key?(seq) then
+            return $cache[record][seq]
+        end
+    end
+    cache_solution = solve_line(record,seq)
+    add_cache(record,seq,cache_solution)
+    return cache_solution
 end
 
 def solve_line(record,seq)
@@ -46,21 +57,14 @@ def solve_line(record,seq)
             return 1
         end
     end
-    if $cache.has_key?(record) then
-        if $cache[record].has_key?(seq) then
-            return $cache[record][seq]
-        end
-    end
 
     if record[0] == '.' then
         return solve_line(record[1..-1],seq)
     elsif record[0] == '?' then
         point_record = (['.'] + record[1..-1])
-        point_solution = solve_line(point_record,seq)
-        add_cache(point_record,seq,point_solution)
+        point_solution = cache_solve_line(point_record,seq)
         hash_record = (['#'] + record[1..-1])
-        hash_solution = solve_line((['#'] + record[1..-1]),seq)
-        add_cache(hash_record,seq,hash_solution)
+        hash_solution = cache_solve_line(hash_record,seq)
         return  point_solution + hash_solution
     elsif record[0] == '#' then
         if record.length >= seq[0] and (not record[0..(seq[0]-1)].include?('.')) then
@@ -68,10 +72,10 @@ def solve_line(record,seq)
                 if record[seq[0]] == '#' then # +1 -1
                     return 0
                 else
-                    return solve_line(record[(seq[0]+1)..-1],seq[1..-1]) 
+                    return cache_solve_line(record[(seq[0]+1)..-1],seq[1..-1]) 
                 end
             end
-            return solve_line(record[seq[0]..-1],seq[1..-1])
+            return cache_solve_line(record[seq[0]..-1],seq[1..-1])
         else
             return 0
         end
